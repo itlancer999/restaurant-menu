2a7a6e419c4f9b711325e056464d9bd42a4e0293
maryam <it.lancer999@gmail.com>
2021-03-17 14:49:42 -0400

    remove unused code

--- src/components/menu.js
+++ src/components/menu.js
@@ -131,56 +131,7 @@ const CardDetail = () => (
 		    	</tr>
 		    </tbody>
 		    </table>
-{/*
-		    <div className="col-6 text-right">
-		    <p className="font-weight-bold money">$9</p>
-		    <p className="font-weight-bold money">$8</p>
-		    <p className="font-weight-bold money">$11.5</p>
-		    <p className="font-weight-bold money">$7</p>
-		    </div>
-		  
-		</div>
 
-		<h2 className="card__detail--title-2 pb-3 pt-5">Desserts</h2>
-
-		<div className="row">
-		  
-		    
-
-		    <div className="col-6">
-		    <p>Jamaican ginger and caramel cake</p>
-		    <p>Teff, banana and apricot teao loaf with hazelnut streusel topping</p>
-		    
-		    </div>
-
-		    <div className="col-6 text-right money">
-		    <p className="font-weight-bold">$3</p>
-		    <p className="font-weight-bold">$2.5</p>
-		    </div>
-		 
-		</div>
-
-		<h2 className="card__detail--title-3 pb-3 pt-5">Drinks</h2>
-
-		<div className="row">
-		  
-		    
-
-		    <div className="col-6">
-		    <p>Virgin Watermelon Sangria</p>
-		    <p>Classic Mimosas</p>
-		    <p>Bellini</p>
-		    <p>Orange and cranberry juice</p>
-		    </div>
-
-		    <div className="col-6 text-right money">
-		    <p className="font-weight-bold">$2.5</p>
-		    <p className="font-weight-bold">$3</p>
-		    <p className="font-weight-bold">$2</p>
-		    <p className="font-weight-bold">$2</p>
-		    </div>
-		  
-		</div>*/}
     </div>
 )
 
